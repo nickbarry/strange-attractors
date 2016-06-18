099d0810a081a2d74f3c98308c3e5e4fb53aa673
Nick Barry <niko.barry@gmail.com>
2016-06-18 12:22:15 -0700

    Add boilerplate test file

---
+++ test/boilerplate.test.js
@@ -0,0 +1,10 @@
+// Boilerplate test stuff here
+var assert = chai.assert;
+
+describe('Array methods', function(){
+  it('should start empty', function(){
+    var arr = [];
+
+    assert.equal(arr.length, 0);
+  });
+});
