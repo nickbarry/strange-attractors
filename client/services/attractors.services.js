5ca7772059cba9f686eecd455dad40ffa64fe9fb
Nick Barry <niko.barry@gmail.com>
2016-08-30 22:27:50 -0700

    refactor: Move attractor tests and utility functions into factories
    I'm making the code more modular, since I'm about to start writing a bunch of new functions that test the interestingness of attractors.

--- client/services/attractors.services.js
+++ client/services/attractors.services.js
@@ -1,10 +1,5 @@
 angular.module('attractors.services', [])
-.factory('Attractors', function() {
-  function roundTo(num, sigDig) {
-    const placesMultiplier = Math.pow(10, sigDig);
-    return Math.round((num * placesMultiplier)) / placesMultiplier;
-  }
-
+.factory('Attractors', function(Tests, Utils) {
   const DEFAULT_POINTS = 500;
 
   function newCoeffs() {
@@ -14,25 +9,6 @@ angular.module('attractors.services', [])
     }
     return coeffs;
   }
-
-  function newVettedCoeffs() {
-    let coeffs
-  }
-
-  function isSetInteresting(data) {
-    // If the data is in a loop (we check up to a period of 25), return false (periodic == uninteresting)
-    const REPEAT_CHECK_PERIOD = 25;
-    const SIG_DIGS_TO_CHECK = 6;
-    const recentXVals = data.slice(-REPEAT_CHECK_PERIOD, -1).map(point => roundTo(point.x, SIG_DIGS_TO_CHECK));
-    const lastXVal = roundTo(data[data.length - 1].x, SIG_DIGS_TO_CHECK);
-    if (~recentXVals.indexOf(lastXVal)) {
-      return false;
-    }
-
-    return true; // If it didn't fail any tests, then assume it's ok
-  }
-
-
 
   function generateData(coeffs, numPoints = DEFAULT_POINTS, data) {
     // If they already gave us data, copy it (or truncate it). Otherwise, create an initial point.
@@ -71,11 +47,10 @@ angular.module('attractors.services', [])
     'WNCSLFLGIHGL',
   ].map(str => (
     [...str].map(ltr => (
-      roundTo(((ltr.charCodeAt(0) - 65) / 10 - 1.2), 1)
+      Utils.roundTo(((ltr.charCodeAt(0) - 65) / 10 - 1.2), 1)
     ))
   ));
   const coolCoeffs = [
-    ...coolCoeffsFromLetters,
     [-0.06310263014972994,-1.001172645052796,0.20860604594503918,-1.0232552434342843,0.09906923627046238,0.08284318930795331,-0.06400291892559928,0.4281973659008149,1.0125745634590657,-0.10628945725110683,0.865172399892385,-0.08486024657845226],
     [-1.0318129217647105,0.9214841230559745,0.3661454509851769,0.40894887515391276,-0.6707172703270613,-0.44227015904617695,0.3016114276861641,-0.07157719343884317,-0.598524097441637,0.2928894024027735,-0.8896046902556218,-0.8108188802701117],
     [-0.5276659662280693,-1.0312667275370713,0.12636687163566718,-0.46804494779618966,0.13923392298309412,-0.8986240965155039,0.38737210566479385,-0.29006571554865346,-0.3408429257638712,0.9228589733810846,-0.04984135776411103,-0.9093024635211264],
@@ -84,6 +59,7 @@ angular.module('attractors.services', [])
     [-0.5715646091513168,-0.15287739181565652,0.2570894262113397,0.987155989918074,0.9788805880806926,-0.6766416102195076,0.7077511335379509,-0.9232330996022602,-0.8473575614112865,0.6731382590236001,0.3637135609769131,0.22832514434172202],
     [0.8807277110844918,-0.7603641468307958,-0.5876182936070823,0.5977780750867363,0.6095143991055132,-0.3779159129535552,0.26814301917573946,-1.1993714279919379,0.17023567347099067,-0.9390497131400751,0.29464129689373975,0.4305979964055793],
     [-0.15466739377215477,-1.039356142360215,0.25752668896969855,1.1846790962331848,-0.24376368781466307,-0.7570023306895022,0.5670078114132497,-0.32725762683488124,-0.6004567237075417,-0.8281224453861642,0.12758550153709014,-0.09495650420828872],
+    ...coolCoeffsFromLetters,
   ];
   function chooseCoolCoeffsSet(setIndex) {
     return (setIndex === undefined) ?
@@ -122,7 +98,7 @@ angular.module('attractors.services', [])
         countGoesToInfinity++;
         continue;
       }
-      if (!isSetInteresting(initialData)) {
+      if (!Tests.isSetInteresting(initialData)) {
         countBoringSet++;
         continue;
       }
@@ -144,114 +120,28 @@ angular.module('attractors.services', [])
   return {
     getAttractors,
   };
+})
+.factory('Tests', function(Utils) {
+  function isSetInteresting(data) {
+    // If the data is in a loop (we check up to a period of 25), return false (periodic == uninteresting)
+    const REPEAT_CHECK_PERIOD = 25;
+    const SIG_DIGS_TO_CHECK = 6;
+    const recentXVals = data.slice(-REPEAT_CHECK_PERIOD, -1).map(point => Utils.roundTo(point.x, SIG_DIGS_TO_CHECK));
+    const lastXVal = Utils.roundTo(data[data.length - 1].x, SIG_DIGS_TO_CHECK);
+    if (~recentXVals.indexOf(lastXVal)) {
+      return false;
+    }
 
-  //function Attractor(coeffs, pointsDesired = DEFAULT_POINTS) {
-  //
-  //  function letterToCoeff(ltr) {
-  //    return roundTo(((ltr.charCodeAt(0) - 65) / 10 - 1.2), 1);
-  //  }
-  //
-  //  if (typeof coeffs === 'string') { // If they entered something like 'AMTMNQQXUYGA', convert to array of numbers
-  //    this.coeffs = [...coeffs].map(letterToCoeff);
-  //  } else if (coeffs === undefined || coeffs === null) {
-  //    this.coeffs = Attractor.getPotentiallyInterestingSets(1)[0];
-  //  }
-  //
-  //  // Generate points based on this set of coefficients
-  //  const data = this.generatePoints(coeffs, pointsDesired);
-  //
-  //  Object.assign(this, { coeffs, data });
-  //}
-  //
-  //Attractor.prototype.generatePoints = function (coeffs = this.coeffs,
-  //                                               numPoints = DEFAULT_POINTS,
-  //                                               points = [{ x: 0, y: 0 }]) {
-  //  const cf = coeffs;
-  //
-  //  for (let i = points.length; i < numPoints; i++) {
-  //    const [x0, y0] = [points[i-1].x, points[i-1].y]; // Get previous x/y coords
-  //    const [x1, y1] = [ // Calculate next point
-  //      cf[0]         + cf[1]  * x0 + cf[2]  * x0*x0 +
-  //      cf[3] * x0*y0 + cf[4]  * y0 + cf[5]  * y0*y0,
-  //      cf[6]         + cf[7]  * x0 + cf[8]  * x0*x0 +
-  //      cf[9] * x0*y0 + cf[10] * y0 + cf[11] * y0*y0
-  //    ];
-  //    if (isNaN(x1) || isNaN(y1)) {
-  //      break; // Exit the loop; don't add the new points to `points`
-  //    }
-  //    points[i] = { x: x1, y: y1 };
-  //  }
-  //
-  //  return points;
-  //};
-  //
-  //Attractor.generateCoolAttractor = function(numPoints = DEFAULT_POINTS) {
-  //  const coolSets = [
-  //    'UWACXDQIGKHF',
-  //    'AMTMNQQXUYGA',
-  //    'CVQKGHQTPHTE',
-  //    'FIRCDERRPVLD',
-  //    'GLXOESFTTPSV',
-  //    'GIIETPIQRRUL',
-  //    'GXQSNSKEECTX',
-  //    'HGUHDPHNSGOH',
-  //    'ILIBVPKJWGRR',
-  //    'LUFBBFISGJYS',
-  //    'MCRBIPOPHTBN',
-  //    'ODGQCNXODNYA',
-  //    'QFFVSLMJJGCR',
-  //    'VBWNBDELYHUL',
-  //    'WNCSLFLGIHGL',
-  //  ];
-  //  const chooseSetNumber = -1;
-  //  const chosenCoeffs = (~chooseSetNumber) ?
-  //    coolSets[chooseSetNumber] :
-  //    coolSets[Math.floor(Math.random() * coolSets.length)];
-  //
-  //  return new Attractor(chosenCoeffs, numPoints);
-  //};
-  //
-  //Attractor.generateCoeffs = function () {
-  //  const coeffs = [];
-  //  for (let i = 0; i < 12; i++) {
-  //    coeffs[i] = Math.random() * 2.4 - 1.2;
-  //  }
-  //  return coeffs;
-  //};
-  //
-  //Attractor.getPotentiallyInterestingSets = function (numSets = 9) {
-  //  const POINTS_TO_TEST = 300;
-  //
-  //  function isSetInteresting(set) {
-  //    // If the data is in a loop (we check up to a period of 25)
-  //    const REPEAT_CHECK_PERIOD = 25;
-  //    if (~set.data.slice(-REPEAT_CHECK_PERIOD).indexOf(set.data[set.data.length - 1])) {
-  //      return false;
-  //    }
-  //
-  //    return true;
-  //  }
-  //
-  //  let trialCounter = 0;
-  //  const sets = [];
-  //
-  //  while (trialCounter++ < 1000 && sets.length < numSets) {
-  //    const randomCoeffs = Attractor.generateCoeffs();
-  //    const set = new Attractor(randomCoeffs, POINTS_TO_TEST);
-  //    (set.data.length >= POINTS_TO_TEST) && isSetInteresting(set) && sets.push(set);
-  //  }
-  //
-  //  return sets;
-  //};
-  //
-  //Attractor.getPotentiallyInterestingAttractors = (num = 9, numPoints = 10000) => {
-  //  const sets = Attractor.getPotentiallyInterestingSets(num);
-  //
-  //  return sets.map(set => new Attractor(set, numPoints));
-  //};
-  //
-  //return {
-  //  roundTo,
-  //  Attractor,
-  //};
+    return true; // If it didn't fail any tests, then assume it's ok
+  }
+
+  return { isSetInteresting };
+})
+.factory('Utils', function() {
+  function roundTo(num, sigDig) {
+    const placesMultiplier = Math.pow(10, sigDig);
+    return Math.round((num * placesMultiplier)) / placesMultiplier;
+  }
+
+  return { roundTo };
 });
